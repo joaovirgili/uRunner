c2e10d174a7c612cce210e699c10a4a95a07d82a
joaovirgili <sr.joaovirgili@gmail.com>
2017-08-26 21:45:24 -0300

    API dos correios funcionando
    A busca do CEP não é automática.
    Adicionado um botão ao lado do CEP para fazer a busca do CEP

--- js/cep.js
+++ js/cep.js
@@ -1,17 +1,26 @@
-var xhr = new XMLHttpRequest();
-var cep = "41810750";
-var linkCEP = "http://viacep.com.br/ws/" +cep+"/json/";
-console.log(linkCEP);
+var buscaCep = document.querySelector("#buscaCep");
+buscaCep.addEventListener("click", function (){
+	var xhr = new XMLHttpRequest();
 
-xhr.open("GET", linkCEP, true);
-xhr.addEventListener("load", function(e, grupos) {
-var resposta = xhr.responseText;
-	if (xhr.status == 200) {
-		dadosCEP = JSON.parse(resposta);
-		console.log(dadosCEP.logradouro);
-		console.log(dadosCEP.bairro);
-		console.log(dadosCEP.localidade);
-		console.log(dadosCEP.uf);
-	}
+	var cep = document.querySelector("#cep");
+	console.log(cep.value);
+
+	var linkCEP = "http://viacep.com.br/ws/" +cep.value+"/json/";
+	console.log(linkCEP);
+	
+	xhr.open("GET", linkCEP, true);
+	xhr.addEventListener("load", function(e, grupos) {
+	var resposta = xhr.responseText;
+		if (xhr.status == 200) {
+			dadosCEP = JSON.parse(resposta);
+			var rua = document.getElementById("rua");
+			rua.value = dadosCEP.logradouro;
+			var bairro = document.getElementById("bairro");
+			bairro.value = dadosCEP.bairro;
+			var cidade = document.getElementById("cidade");
+			cidade.value = dadosCEP.localidade;
+		}
+	});
+	xhr.send();
 });
-xhr.send();
+
