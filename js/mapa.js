5465c7d2bade8dbce35aa135dbcdfd5897d8a299
joaovirgili <sr.joaovirgili@gmail.com>
2017-08-27 23:22:48 -0300

    Ajuste FInal
    Adicionada página para ver grupos cadastrados

--- js/mapa.js
+++ js/mapa.js
@@ -1,10 +1,12 @@
 var map;
+var markers = [];
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
           center: {lat:-12.998514, lng:-38.451004},
           zoom: 13
-        });
+		});
+	
 
 	function criaMarker(grupo) {
 
@@ -19,6 +21,7 @@ function initMap() {
 			map: map,
 			title: grupo.nome
 		})
+		markers.push(marker);
 		marker.addListener('click', function() {
 			infowindow.open(map, marker);
 		});
@@ -31,10 +34,50 @@ function initMap() {
 		var resposta = xhr.responseText;
 		if (xhr.status == 200) {
 			grupos = JSON.parse(resposta);
+
+			//criar os marcadores
 			for (let i=0; i<grupos.length; i++) {
 				grupo = grupos[i];
 				criaMarker(grupo);
 			}
+
+			//adicionar os botoes na pagina que mostra os grupos
+			for (var i=0;i<grupos.length;i++) {
+				var li = document.createElement("li");
+				var ul = document.querySelector("#lista");
+		
+				li.classList.add("grupos");
+				li.classList.add("botao");
+				li.textContent = grupos[i].nome;
+				ul.appendChild(li);
+			}
+			
+			function getContent (botao) {
+				for (var i=0;i<grupos.length;i++) {
+					if (botao.textContent == grupos[i].nome) 
+						return grupos[i].conteudo;
+				}
+			}
+
+			var ul = document.querySelector("#lista");
+			ul.addEventListener("click", function (event) {
+				event.preventDefault();
+				if (event.target.nodeName == "LI") {
+					botao = event.target;
+					conteudo = getContent(botao);
+					for (var i=0; i<markers.length;i++) {
+						marker = markers[i];
+						markerName = markers[i].title;
+						if (botao.textContent == markerName) {
+							var infowindow = new google.maps.InfoWindow({content: conteudo});
+							infowindow.close();
+							infowindow.open(map, markers[i]);
+						}
+							
+					}
+				}
+			});
+			
 		}
 	});
 	xhr.send();
